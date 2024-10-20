86a2ee2a163b21448b49525968d1e988e8231342
KominPPP <147441884+KominPPP@users.noreply.github.com>
2024-10-20 19:56:25 +0500

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+const modal =() => {
+const modal = document.querySelector('.search-model');
+const modalBtn = document.querySelector('.icon_search');
+const modalClose = modal.querySelector('.search-close-switch');
+
+
+modalBtn.addEventListener('click', () => {
+modal.style.display = 'block'
+});
+
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none'
+    });
+
+
+const search = () => {
+    const input = modal.querySelector('.search-model-form > input');
+    input.addEventListener('input', (e) => {
+    console.log(input.value)
+})
+}
+
+search();
+}
+
+modal()
+
